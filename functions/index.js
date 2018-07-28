0079e14174d29617f1a6993cca6ecb97ee5a4905
Muhammad Abubakar Siddiq <mas.abbe@gmail.com>
2018-07-28 09:56:59 +0700

    handlebarsError

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
+const admin		= require('firebase-admin');
 const express	= require('express');
+const engines	= require('consolidate');
 
 const app		= express();
 
+const firebaseApp	= admin.initializeApp(
+	functions.config().admin
+);
+
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
 app.get('/time', (req, res)=>{
-	res.send('${Date.now()}')
+	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	res.render('index')
 });
 exports.app = functions.https.onRequest(app);
