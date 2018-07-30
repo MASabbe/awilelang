52310ec4ca36d8821d6f7040fb8230d705aebac8
Muhammad Abubakar Siddiq <mas.abbe@gmail.com>
2018-07-30 22:38:20 +0700

    2push

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ const app		= express();
 const firebaseApp	= admin.initializeApp(
 	functions.config().admin
 );
+const HTTP_SERVER_ERROR = 500;
 
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
@@ -16,5 +17,13 @@ app.set('view engine', 'hbs');
 app.get('/time', (req, res)=>{
 	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
 	res.render('index')
+});
+
+app.use(function(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  return res.status(err.status || HTTP_SERVER_ERROR).render('500');
 });
 exports.app = functions.https.onRequest(app);
