b5d8e0841f1905a1c44fa4c79312077fc7895c15
Muhammad Abubakar Siddiq <mas.abbe@gmail.com>
2018-08-01 01:06:42 +0700

    tambah angular

--- functions/index.js
+++ functions/index.js
@@ -2,22 +2,53 @@ const functions = require('firebase-functions');
 const admin		= require('firebase-admin');
 const express	= require('express');
 const engines	= require('consolidate');
+const path   	= require('path');
+const session 	= require('express-session');
+const body 		= require('body-parser');
 
 const app		= express();
 
 const firebaseApp	= admin.initializeApp(
 	functions.config().admin
 );
+
 const HTTP_SERVER_ERROR = 500;
 
 app.engine('hbs', engines.handlebars);
 app.set('views', './views');
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res)=>{
+
+app.get('/admin/', (req, res)=>{
 	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
 	res.render('index')
 });
+
+app.post('/admin/', (req, res)=>{
+	var useremail = req.body.username;
+	var userpassword  = req.body.userpass;
+
+	admin.auth().signInWithEmailAndPassword(useremail, userpassword)
+	.then(function () {
+		res.send("success");
+		console.log("success login admin".useremail);
+	})
+	.catch(function(err){
+		res.send("fail");
+		console.log("Error while executing firebase.auth() ",err);
+	});
+});
+
+app.get('/admin/home', (req, res)=>{
+	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	res.render('dashboard');
+});
+
+app.get('/', (req, res)=>{
+	res.set('Cache-Control', 'public, max-age=300, s-maxage=600');
+	res.render('home');
+});
+
 
 app.use(function(err, req, res, next) {
   if (res.headersSent) {
---
+++ public/javascripts/admin.js
@@ -0,0 +1,10 @@
+$(function () {	
+	'usestrict';
+
+	var admin = angular.module("myAdmin", ["ngRoute"]);
+
+	admin.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+		// body...
+	}]);
+
+});
