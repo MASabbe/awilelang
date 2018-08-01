5c3db29f5db8824407f6fd34b626db323c0f589e
Muhammad Abubakar Siddiq <mas.abbe@gmail.com>
2018-08-02 01:23:52 +0700

    fixed error module

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,15 +1,17 @@
 (function () {
-	'usestrict';
-
-	angular.module("myAdmin").controller('loginCtrl', function($scope, $http){
-		$scope.login = function() {
-        	$http.post('/admin/',{useremail:$scope.userEmail, userpassword:$scope.userPassword}).
-        		then(function(response) {
-            	console.log("posted successfully");
-            	window.location.href="/admin/home";
-        	}).catch(function(response) {
-            	console.error("error in posting");
-        	})
-    	}	
-	}
-)();
+    'usestrict';
+"the initialize angular module"
+    angular.module("myAdmin",[]).controller('loginCtrl', function($scope, $http){
+        $scope.login = function() {
+            $http.post('/admin/',{useremail:$scope.userEmail, userpassword:$scope.userPassword}).
+                then(function(response) {
+                console.log("posted successfully");
+                window.location.href="/admin/home";
+            }).catch(function(response) {
+                console.error("error in posting");
+            })
+        }   
+    }
+)
+}) // <- theses bracket and parenthesis are missing
+();
